ee334e9884597dfc24a9b6e69b7401c5ce452c68
LeDinhQuoc24 <kudo2412@gmail.com>
2020-06-09 11:14:28 +0700

    up_entity

--- dao/src/main/java/com/codegym/dao/entity/Floor.java
+++ dao/src/main/java/com/codegym/dao/entity/Floor.java
@@ -1,4 +1,108 @@
 package com.codegym.dao.entity;
 
+import javax.persistence.*;
+
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+@Table(name = "floor")
 public class Floor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nameFloor;
+    private String codeFloor;
+    private double area;
+    private String capacity;
+    private String statusFloor;
+    private Integer deleteFlag;
+    private String typeFloor;
+    @ManyToOne(targetEntity = Building.class)
+    @JoinColumn(name="building_id")
+    private Building building;
+    @OneToMany(targetEntity = Ground.class,cascade = ALL)
+    @JoinColumn(name="ground_id")
+    private List<Ground> grounds;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNameFloor() {
+        return nameFloor;
+    }
+
+    public void setNameFloor(String nameFloor) {
+        this.nameFloor = nameFloor;
+    }
+
+    public String getCodeFloor() {
+        return codeFloor;
+    }
+
+    public void setCodeFloor(String codeFloor) {
+        this.codeFloor = codeFloor;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getStatusFloor() {
+        return statusFloor;
+    }
+
+    public void setStatusFloor(String statusFloor) {
+        this.statusFloor = statusFloor;
+    }
+
+    public Integer getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Integer deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+    public String getTypeFloor() {
+        return typeFloor;
+    }
+
+    public void setTypeFloor(String typeFloor) {
+        this.typeFloor = typeFloor;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
+
+    public List<Ground> getGrounds() {
+        return grounds;
+    }
+
+    public void setGrounds(List<Ground> grounds) {
+        this.grounds = grounds;
+    }
 }
--- dao/src/main/java/com/codegym/dao/entity/Image.java
+++ dao/src/main/java/com/codegym/dao/entity/Image.java
@@ -1,4 +1,62 @@
 package com.codegym.dao.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+@Table(name = "image")
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nameImage;
+    private String urlImage;
+    private Integer deleteFlag;
+
+
+    @OneToMany(targetEntity = Building.class,cascade = ALL)
+    @JoinColumn(name="building_id")
+    private List<Building> buildings;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public List<Building> getBuildings() {
+        return buildings;
+    }
+
+    public void setBuildings(List<Building> buildings) {
+        this.buildings = buildings;
+    }
+
+    public String getNameImage() {
+        return nameImage;
+    }
+
+    public void setNameImage(String nameImage) {
+        this.nameImage = nameImage;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public Integer getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Integer deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
 }
+
