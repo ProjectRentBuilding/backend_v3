8a056d25aac1f5cb49a40154340a564503209317
Minh Hùng Võ <nguyengiacat1@gmail.com>
2020-06-09 14:02:03 +0700

    update

--- dao/src/main/java/com/codegym/dao/entity/Equipment.java
+++ dao/src/main/java/com/codegym/dao/entity/Equipment.java
@@ -8,7 +8,7 @@ public class Equipment {
     @Id
     @Column(name = "id_equipment")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Integer id;
 
     @Column(name = "name_equipment")
     private String nameEquipment;
@@ -25,10 +25,89 @@ public class Equipment {
     @Column(name = "note")
     private String note;
 
-    @ManyToOne(targetEntity = TypeEquipment.class)
+    @ManyToOne
     @JoinColumn(name = "id_type")
-    private Integer idType;
+    private TypeEquipment typeEquipment;
+
+    @ManyToOne
+    @JoinColumn(name = "id_ground")
+    private Integer idGround;
 
     @Column(name = "delete_flag")
     private Integer deleteFlag;
+
+    public Equipment() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNameEquipment() {
+        return nameEquipment;
+    }
+
+    public void setNameEquipment(String nameEquipment) {
+        this.nameEquipment = nameEquipment;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getStatusEquipment() {
+        return statusEquipment;
+    }
+
+    public void setStatusEquipment(String statusEquipment) {
+        this.statusEquipment = statusEquipment;
+    }
+
+    public Integer getAmountOfBroken() {
+        return amountOfBroken;
+    }
+
+    public void setAmountOfBroken(Integer amountOfBroken) {
+        this.amountOfBroken = amountOfBroken;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public TypeEquipment getTypeEquipment() {
+        return typeEquipment;
+    }
+
+    public void setTypeEquipment(TypeEquipment typeEquipment) {
+        this.typeEquipment = typeEquipment;
+    }
+
+    public Integer getIdGround() {
+        return idGround;
+    }
+
+    public void setIdGround(Integer idGround) {
+        this.idGround = idGround;
+    }
+
+    public Integer getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Integer deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
 }
