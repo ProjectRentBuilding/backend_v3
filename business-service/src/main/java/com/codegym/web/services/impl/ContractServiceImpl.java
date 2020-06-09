70de7131f9d927a7006e55286a48e58489f8c95c
Admin <dinhhoa1994@gmail.com>
2020-06-09 15:29:04 +0700

    df

--- business-service/src/main/java/com/codegym/web/services/impl/ContractServiceImpl.java
+++ business-service/src/main/java/com/codegym/web/services/impl/ContractServiceImpl.java
@@ -1,4 +1,24 @@
 package com.codegym.web.services.impl;
 
-public class ContractServiceImpl {
+import com.codegym.dao.entity.Contract;
+import com.codegym.dao.repository.ContractRepository;
+import com.codegym.web.services.ContractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContractServiceImpl implements ContractService {
+
+
+    @Autowired
+    ContractRepository contractRepository;
+
+
+    @Override
+    public List<Contract> findAllByDeleteFlagIsNull() {
+        return contractRepository.findAllByDeleteFlagIsNull();
+    }
+
 }
--- web-service/src/main/java/com/codegym/webservice/controller/ContractController.java
+++ web-service/src/main/java/com/codegym/webservice/controller/ContractController.java
@@ -1,4 +1,20 @@
 package com.codegym.webservice.controller;
 
+import com.codegym.web.services.ContractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/contract")
 public class ContractController {
+
+    @Autowired
+    ContractService contractService;
+//
+//    @GetMapping("")
+//    public
 }
