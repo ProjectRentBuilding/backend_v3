381d504c6f3e1a07ad4c7a7d31fe0c7ce7037586
Minh Hùng Võ <nguyengiacat1@gmail.com>
2020-06-10 17:21:27 +0700

    hung up

--- business-service/src/main/java/com/codegym/web/services/EquipmentService.java
+++ business-service/src/main/java/com/codegym/web/services/EquipmentService.java
@@ -1,12 +1,16 @@
 package com.codegym.web.services;
 
-import com.codegym.dao.dto.ContractDTO;
-import com.codegym.dao.entity.Contract;
+import com.codegym.dao.dto.EquipmentDTO;
 import com.codegym.dao.entity.Equipment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface EquipmentService {
     List<Equipment> findAllByDeleteFlagIsNull();
-//    EquipmentDTO findAllByDeleteFlagIsNullAndIdIs(Integer id);
+    EquipmentDTO findAllByDeleteFlagIsNullAndIdIs(Integer id);
+    void save(Equipment equipment);
+    Page<Equipment> findAllByDeleteFlagIsNullAndTypeEquipment(Pageable pageable, String typeEquipment);
+    Page<Equipment> findAllByDeleteFlagIsNullAndGround(Pageable pageable, String ground);
 }
--- web-service/src/main/java/com/codegym/webservice/controller/EquipmentController.java
+++ web-service/src/main/java/com/codegym/webservice/controller/EquipmentController.java
@@ -23,4 +23,6 @@ public class EquipmentController {
         equipments = equipmentService.findAllByDeleteFlagIsNull();
         return equipments;
     }
+
+
 }
