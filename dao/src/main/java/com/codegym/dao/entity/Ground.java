7689a008f004dba032e8bbe53a6780941cf5373a
LeDinhQuoc24 <kudo2412@gmail.com>
2020-06-10 08:27:14 +0700

    fix_entity_v3

--- dao/src/main/java/com/codegym/dao/entity/Ground.java
+++ dao/src/main/java/com/codegym/dao/entity/Ground.java
@@ -52,5 +52,91 @@ public class Ground {
 
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodeGround() {
+        return codeGround;
+    }
+
+    public void setCodeGround(String codeGround) {
+        this.codeGround = codeGround;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public String getStatusGround() {
+        return statusGround;
+    }
+
+    public void setStatusGround(String statusGround) {
+        this.statusGround = statusGround;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getPriceManager() {
+        return priceManager;
+    }
+
+    public void setPriceManager(Integer priceManager) {
+        this.priceManager = priceManager;
+    }
+
+    public Integer getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Integer deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+    public Floor getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Floor floor) {
+        this.floor = floor;
+    }
+
+    public Set<Equipment> getEquipments() {
+        return equipments;
+    }
+
+    public void setEquipments(Set<Equipment> equipments) {
+        this.equipments = equipments;
+    }
+
+    public Set<Contract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(Set<Contract> contracts) {
+        this.contracts = contracts;
+    }
+
+    public TypeGround getTypeGround() {
+        return typeGround;
+    }
+
+    public void setTypeGround(TypeGround typeGround) {
+        this.typeGround = typeGround;
+    }
 }
