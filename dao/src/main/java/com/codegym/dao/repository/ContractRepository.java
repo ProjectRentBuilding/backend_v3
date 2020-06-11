e60adae9737251b436cd4b8e3c40db3927b7aa77
Admin <dinhhoa1994@gmail.com>
2020-06-11 17:23:47 +0700

    fdf

--- dao/src/main/java/com/codegym/dao/repository/ContractRepository.java
+++ dao/src/main/java/com/codegym/dao/repository/ContractRepository.java
@@ -1,12 +1,17 @@
 package com.codegym.dao.repository;
 
 import com.codegym.dao.entity.Contract;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Integer> {
     List<Contract> findAllByDeleteFlagIsNull();
     Contract findAllByDeleteFlagIsNullAndIdIs(Integer id);
+    Page<Contract> findAllByDeleteFlagIsNull(Pageable pageable);
+    Page<Contract> findAllByDeleteFlagIsNullAndCustomerNameIs(String fullName,Pageable pageable);
 }
