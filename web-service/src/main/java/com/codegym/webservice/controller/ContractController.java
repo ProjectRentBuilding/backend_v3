02e22379d575b09c988e1dc1c0137bbbf5db2561
Admin <dinhhoa1994@gmail.com>
2020-06-13 09:53:16 +0700

    dfdf

--- web-service/src/main/java/com/codegym/webservice/controller/ContractController.java
+++ web-service/src/main/java/com/codegym/webservice/controller/ContractController.java
@@ -53,10 +53,7 @@ public class ContractController {
 
 
     @PutMapping("/{id}")
-//    public ResponseEntity<ContractDTO> updateContract(@PathVariable(value = "id") Integer id ,@RequestBody ContractDTO contractDTO){
-    public ResponseEntity<ContractDTO> updateContract(@PathVariable(value = "id") Integer id ,@RequestBody ContractDTO contractDTO){
-//        ContractDTO contractDTO = contractService.findAllByDeleteFlagIsNullAndIdIs(id);
-        System.out.println(contractDTO.getId()+" null o day");
+    public ResponseEntity<ContractDTO> updateContract(@PathVariable(value = "id") Integer id, @RequestBody ContractDTO contractDTO) {
         contractService.updateContract(contractDTO);
         return ResponseEntity.ok(contractDTO);
     }
