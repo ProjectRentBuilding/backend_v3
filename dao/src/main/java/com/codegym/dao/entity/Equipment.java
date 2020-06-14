14562ecbfb80c683af653f40c5ea88ccd97f5593
Minh Hùng Võ <nguyengiacat1@gmail.com>
2020-06-14 12:01:25 +0700

    hung da up

--- dao/src/main/java/com/codegym/dao/entity/Equipment.java
+++ dao/src/main/java/com/codegym/dao/entity/Equipment.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Equipment {
     @Id
     @Column(name = "id_equipment")
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "name_equipment")
