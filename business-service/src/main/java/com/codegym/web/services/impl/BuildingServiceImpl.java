e4eba47ac98d59b9f02e2a276c58d9d787e3b3eb
LeDinhQuoc24 <kudo2412@gmail.com>
2020-06-16 13:11:06 +0700

    fix_v22_final_spring1

--- business-service/src/main/java/com/codegym/web/services/impl/BuildingServiceImpl.java
+++ business-service/src/main/java/com/codegym/web/services/impl/BuildingServiceImpl.java
@@ -5,6 +5,8 @@ import com.codegym.dao.entity.Building;
 import com.codegym.dao.repository.BuildingRepository;
 import com.codegym.web.services.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,6 +42,11 @@ public class BuildingServiceImpl implements BuildingService {
             return buildingDTO;
         }
         return null;
+    }
+
+    @Override
+    public Page<Building> getBuildings(String fullName, Pageable pageable) {
+        return buildingRepository.findAllByDeleteFlagIsNullAndFullNameContaining(fullName, pageable);
     }
 
     @Override
