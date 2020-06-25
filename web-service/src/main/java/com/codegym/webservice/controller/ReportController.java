1afd61463fdd5cb218c4cc005ec267ee10ee003d
Admin <dinhhoa1994@gmail.com>
2020-06-25 10:43:26 +0700

    dfgfg

--- web-service/src/main/java/com/codegym/webservice/controller/ReportController.java
+++ web-service/src/main/java/com/codegym/webservice/controller/ReportController.java
@@ -9,12 +9,13 @@ import com.codegym.web.services.ContractService;
 import com.codegym.web.services.GroundService;
 import com.codegym.web.services.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -44,6 +45,17 @@ public class ReportController {
     @GetMapping("/low")
     public List<ReportInt> getReportLow() {
         return reportService.searchAllReportLow();
+    }
+
+
+    @GetMapping(value = "/search", params = {"startRentDay", "endRentDay", "minTotal", "maxTotal", "codeGround"})
+    public List<ReportInt> getListReport(@RequestParam(value = "startRentDay", defaultValue = "1900-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startRentDay,
+                                         @RequestParam(value = "endRentDay", defaultValue = "2030-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endRentDay,
+                                         @RequestParam(value = "minTotal", defaultValue = "0") Double minTotal,
+                                         @RequestParam(value = "maxTotal", defaultValue = "99999999999") Double maxTotal,
+                                         @RequestParam(value = "codeGround", defaultValue = "") String codeGround) {
+        List<ReportInt> reportInts = reportService.searchReportWithAny(startRentDay, endRentDay, minTotal, maxTotal, codeGround);
+        return reportService.searchReportWithAny(startRentDay, endRentDay, minTotal, maxTotal, codeGround);
     }
 
     //    @GetMapping("/all")
