4f3deb84f93c01b32d813acdab74e1487c176af4
tranngoctan16t5 <49084352+tranngoctan16t5@users.noreply.github.com>
2020-06-29 13:52:08 +0700

    tan up

--- business-service/src/main/java/com/codegym/web/services/EmployeeService.java
+++ business-service/src/main/java/com/codegym/web/services/EmployeeService.java
@@ -3,11 +3,13 @@ package com.codegym.web.services;
 
 import com.codegym.dao.dto.CustomerDTO;
 import com.codegym.dao.dto.EmployeeDTO;
+import com.codegym.dao.entity.Contract;
 import com.codegym.dao.entity.Customer;
 import com.codegym.dao.entity.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Date;
 import java.util.List;
 
 public interface EmployeeService {
@@ -19,4 +21,5 @@ public interface EmployeeService {
     void save(EmployeeDTO customerDTO);
     void updateEmployee(EmployeeDTO employeeDTO);
     Page<Employee> getEmployees(String nameEmployee, Pageable pageable);
+    Page<Employee> searchAnything(String name, String idCard, String address, String part, Pageable pageable);
 }
--- business-service/src/main/java/com/codegym/web/services/impl/EmployeeServiceImpl.java
+++ business-service/src/main/java/com/codegym/web/services/impl/EmployeeServiceImpl.java
@@ -2,21 +2,27 @@ package com.codegym.web.services.impl;
 
 import com.codegym.dao.dto.CustomerDTO;
 import com.codegym.dao.dto.EmployeeDTO;
+import com.codegym.dao.entity.Contract;
 import com.codegym.dao.entity.Customer;
 import com.codegym.dao.entity.Employee;
 import com.codegym.dao.repository.EmployeeRepository;
+import com.codegym.dao.repository.UserBuildingRepository;
 import com.codegym.web.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
+
+    @Autowired
+    UserBuildingRepository userBuildingRepository;
 
     @Override
     public List<Employee> findAllByDeleteFlagIsNull() {
@@ -26,7 +32,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeDTO getEmployeeById(Integer id) {
         Employee employee = employeeRepository.findAllByDeleteFlagIsNullAndIdIs(id);
-        if(employee != null){
+        if (employee != null) {
             EmployeeDTO employeeDTO = new EmployeeDTO();
             employeeDTO.setId(employee.getId());
             employeeDTO.setDeleteFlag(employee.getDeleteFlag());
@@ -39,14 +45,15 @@ public class EmployeeServiceImpl implements EmployeeService {
             employeeDTO.setGender(employee.getGender());
             employeeDTO.setLevelSalary(employee.getLevelSalary());
             employeeDTO.setPart(employee.getPart());
+            employeeDTO.setSalary(employee.getSalary());
             employeeDTO.setStartWord(employee.getStartWord());
             employeeDTO.setTypeEmployee(employee.getTypeEmployee());
-            employeeDTO.setUserBuilding(employee.getUserBuilding());
+            employeeDTO.setUserBuilding(employee.getUserBuilding().getUsername());
             employeeDTO.setContracts(employee.getContracts());
-            return  employeeDTO;        }
+            return employeeDTO;
+        }
         return null;
     }
-
 
 
     @Override
@@ -59,23 +66,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public void save(EmployeeDTO employeeDTO) {
-       Employee employee = new Employee();
-       employee.setId(employeeDTO.getId());
-       employee.setDeleteFlag(employeeDTO.getDeleteFlag());
-       employee.setName(employeeDTO.getName());
-       employee.setBirthday(employeeDTO.getBirthday());
-       employee.setIdCard(employeeDTO.getIdCard());
-       employee.setPhone(employeeDTO.getPhone());
-       employee.setEmail(employeeDTO.getEmail());
-       employee.setAddress(employeeDTO.getAddress());
-       employee.setGender(employeeDTO.getGender());
-       employee.setLevelSalary(employeeDTO.getLevelSalary());
-       employee.setPart(employeeDTO.getPart());
-       employee.setStartWord(employeeDTO.getStartWord());
-       employee.setTypeEmployee(employeeDTO.getTypeEmployee());
-       employee.setUserBuilding(employeeDTO.getUserBuilding());
-       employee.setContracts(employeeDTO.getContracts());
-       employeeRepository.save(employee);
+        Employee employee = new Employee();
+        employee.setId(employeeDTO.getId());
+        employee.setDeleteFlag(employeeDTO.getDeleteFlag());
+        employee.setName(employeeDTO.getName());
+        employee.setBirthday(employeeDTO.getBirthday());
+        employee.setIdCard(employeeDTO.getIdCard());
+        employee.setPhone(employeeDTO.getPhone());
+        employee.setEmail(employeeDTO.getEmail());
+        employee.setAddress(employeeDTO.getAddress());
+        employee.setGender(employeeDTO.getGender());
+        employee.setLevelSalary(employeeDTO.getLevelSalary());
+        employee.setPart(employeeDTO.getPart());
+        employee.setStartWord(employeeDTO.getStartWord());
+        employee.setTypeEmployee(employeeDTO.getTypeEmployee());
+        employee.setSalary(employeeDTO.getSalary());
+        employee.setUserBuilding(userBuildingRepository.findAllByDeleteFlagIsNullAndUsernameIs(employeeDTO.getUserBuilding()));
+        employee.setContracts(employeeDTO.getContracts());
+        employeeRepository.save(employee);
 
     }
 
@@ -95,13 +103,21 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPart(employeeDTO.getPart());
         employee.setStartWord(employeeDTO.getStartWord());
         employee.setTypeEmployee(employeeDTO.getTypeEmployee());
-        employee.setUserBuilding(employeeDTO.getUserBuilding());
+        employee.setSalary(employeeDTO.getSalary());
+        employee.setUserBuilding(userBuildingRepository.findAllByDeleteFlagIsNullAndUsernameIs(employeeDTO.getUserBuilding()));
         employee.setContracts(employeeDTO.getContracts());
         employeeRepository.save(employee);
     }
 
     @Override
     public Page<Employee> getEmployees(String nameEmployee, Pageable pageable) {
-        return employeeRepository.findAllByDeleteFlagIsNullAndNameContainingIgnoreCase(nameEmployee,pageable);
+        return employeeRepository.findAllByDeleteFlagIsNullAndNameContainingIgnoreCase(nameEmployee, pageable);
     }
+
+    @Override
+    public Page<Employee> searchAnything(String name, String idCard, String address, String part, Pageable pageable) {
+        return employeeRepository.searchAnything(name,idCard,address,part, pageable);
+    }
+
+
 }
